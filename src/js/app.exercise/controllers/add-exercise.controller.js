3a8d6853ddd1757ce1b652a52f1acb6770aa4fec
Benjie Songsong <benjiesongsong@gmail.com>
2016-06-22 10:27:13 -0400

    working on the exercise library

--- src/js/app.exercise/controllers/add-exercise.controller.js
+++ src/js/app.exercise/controllers/add-exercise.controller.js
@@ -3,6 +3,7 @@ function AddExerciseController (ExerciseService, $state) {
   let vm = this;
   vm.addExercise = addExercise;
   vm.bodyParts = [
+    'Neck',
     'Shoulders',
     'Upper Arms',
     'Forearms',
@@ -10,31 +11,13 @@ function AddExerciseController (ExerciseService, $state) {
     'Waist',
     'Back',
     'Hips',
+    'Glutes',
     'Thighs',
     'Calves'
   ];
 
-  vm.muscles = [
-    'Anterior deltoid',
-    'Biceps brachii',
-    'Biceps femoris',
-    'Brachialis',
-    'Gastrocnemius',
-    'Gluteus maximus',
-    'Latissimus dorsi',
-    'Obliquus externus abdominis',
-    'Pectoralis major',
-    'Quadriceps femoris',
-    'Rectus abdominis',
-    'Serratus anterior',
-    'Soleus',
-    'Trapezius',
-    'Triceps brachii'
-  ];
-
   function addExercise(exercise) {
     exercise.bodyPart = exercise.bodyPart.toString();
-    exercise.muscles = exercise.muscles.toString();
     ExerciseService.add(exercise).then( (res) => {
       alert('Exercise Added!');
       $state.go('root.exercises');
